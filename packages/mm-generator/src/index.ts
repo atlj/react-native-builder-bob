aba7e7eed144b8fef46d5a83eaf6c30cdfa687ff
Burak Güner <burak.guner@callstack.com>
2023-12-01 19:12:54 +0300

    chore: format everything

--- packages/mm-generator/src/index.ts
+++ packages/mm-generator/src/index.ts
@@ -1,101 +1,126 @@
-import * as z from 'zod'
-import {execSync} from 'child_process'
+import * as z from 'zod';
+import { execSync } from 'child_process';
 
-import fs from 'fs-extra'
-import path from 'path'
+import fs from 'fs-extra';
+import path from 'path';
 
-import { type NamedShape, type NativeModuleParamTypeAnnotation, type NativeModuleReturnTypeAnnotation, type Nullable, type SchemaType } from '@react-native/codegen/lib/CodegenSchema'
+import type {
+  NamedShape,
+  NativeModuleParamTypeAnnotation,
+  NativeModuleReturnTypeAnnotation,
+  Nullable,
+  SchemaType,
+} from '@react-native/codegen/lib/CodegenSchema';
 
 const CodegenConfig = z.object({
   name: z.string(),
   jsSrcsDir: z.string(),
-  type: z.union([z.literal('modules'), z.literal('components'), z.literal('all')]),
-})
+  type: z.union([
+    z.literal('modules'),
+    z.literal('components'),
+    z.literal('all'),
+  ]),
+});
 
-type CodegenConfig = z.infer<typeof CodegenConfig>
+type CodegenConfig = z.infer<typeof CodegenConfig>;
 
-function parseCodegenConfig(){
-  const packageJson = fs.readJsonSync(path.join(process.cwd(), 'package.json'))
+function parseCodegenConfig() {
+  const packageJson = fs.readJsonSync(path.join(process.cwd(), 'package.json'));
 
-
-  const codegenConfigParse = CodegenConfig.safeParse(packageJson.codegenConfig)
-  if(!codegenConfigParse.success){
-    throw new Error('Invalid codegenConfig')
+  const codegenConfigParse = CodegenConfig.safeParse(packageJson.codegenConfig);
+  if (!codegenConfigParse.success) {
+    throw new Error('Invalid codegenConfig');
   }
 
-  return codegenConfigParse.data
+  return codegenConfigParse.data;
 }
-
 
 function generateSchema(codegenConfig: CodegenConfig): SchemaType {
-  const schemaParserPath = require.resolve('@react-native/codegen/lib/cli/combine/combine-js-to-schema-cli')
+  const schemaParserPath = require.resolve(
+    '@react-native/codegen/lib/cli/combine/combine-js-to-schema-cli'
+  );
 
-  const generatedSchemaPath = path.join(process.cwd(), 'schemas',`${codegenConfig.name}.schema.json`)
+  const generatedSchemaPath = path.join(
+    process.cwd(),
+    'schemas',
+    `${codegenConfig.name}.schema.json`
+  );
 
-  if(!fs.existsSync(generatedSchemaPath)){
-    fs.ensureFileSync(generatedSchemaPath)
+  if (!fs.existsSync(generatedSchemaPath)) {
+    fs.ensureFileSync(generatedSchemaPath);
   }
 
-  const commandToGenerateSchema = `node ${schemaParserPath} --platform ios ${generatedSchemaPath} ${codegenConfig.jsSrcsDir}`
+  const commandToGenerateSchema = `node ${schemaParserPath} --platform ios ${generatedSchemaPath} ${codegenConfig.jsSrcsDir}`;
 
-  execSync(commandToGenerateSchema)
+  execSync(commandToGenerateSchema);
 
-  return fs.readJsonSync(generatedSchemaPath)
+  return fs.readJsonSync(generatedSchemaPath);
 }
 
+const codegenConfig = parseCodegenConfig();
+const schema = generateSchema(codegenConfig);
 
-const codegenConfig = parseCodegenConfig()
-const schema = generateSchema(codegenConfig)
+for (const key of Object.keys(schema.modules)) {
+  const module = schema.modules[key];
 
-for (const key of Object.keys(schema.modules)){
-  const module = schema.modules[key]
-
-  if(!module || module?.type === 'Component'){
+  if (!module || module?.type === 'Component') {
     // TODO handle views
-    continue
+    continue;
   }
 
-  const properties = module.spec.properties
+  const properties = module.spec.properties;
 
   for (const property of properties) {
-    const result = handleAnnotation(property)
-    console.log(result)
-
+    const result = handleAnnotation(property);
+    console.log(result);
   }
-
 }
 
 // Generate objc code from schema
-function handleAnnotation(param: NamedShape<Nullable<NativeModuleParamTypeAnnotation>> | NamedShape<Nullable<NativeModuleReturnTypeAnnotation>>): string {
+function handleAnnotation(
+  param:
+    | NamedShape<Nullable<NativeModuleParamTypeAnnotation>>
+    | NamedShape<Nullable<NativeModuleReturnTypeAnnotation>>
+): string {
   switch (param.typeAnnotation.type) {
     case 'BooleanTypeAnnotation':
-      return `BOOL`
+      return `BOOL`;
     case 'NullableTypeAnnotation':
-      return `Nullable ${handleAnnotation({typeAnnotation: param.typeAnnotation.typeAnnotation, name: param.name, optional: true})}`
+      return `Nullable ${handleAnnotation({
+        typeAnnotation: param.typeAnnotation.typeAnnotation,
+        name: param.name,
+        optional: true,
+      })}`;
     case 'DoubleTypeAnnotation':
-      return `double`
+      return `double`;
     case 'Int32TypeAnnotation':
-      return `int`
+      return `int`;
     case 'StringTypeAnnotation':
-      return `NSString`
+      return `NSString`;
     case 'NumberTypeAnnotation':
-      return `NSNumber`
+      return `NSNumber`;
     case 'FloatTypeAnnotation':
-      return `float`
+      return `float`;
     case 'VoidTypeAnnotation':
-      return `void`
+      return `void`;
     case 'ArrayTypeAnnotation':
-      return `NSArray`
+      return `NSArray`;
     case 'EnumDeclaration':
-      return `NSInteger`
+      return `NSInteger`;
     case 'ObjectTypeAnnotation':
-      return `NSDictionary`
+      return `NSDictionary`;
     case 'FunctionTypeAnnotation':
-      const params = param.typeAnnotation.params.map((param) => `(${handleAnnotation(param)} *) ${param.name}`).join(': ')
-      const result = `(${handleAnnotation({typeAnnotation: param.typeAnnotation.returnTypeAnnotation, name: param.name, optional: false})}) ${param.name}: ${params}`
-      return result
+      const params = param.typeAnnotation.params
+        .map((param) => `(${handleAnnotation(param)} *) ${param.name}`)
+        .join(': ');
+      const result = `(${handleAnnotation({
+        typeAnnotation: param.typeAnnotation.returnTypeAnnotation,
+        name: param.name,
+        optional: false,
+      })}) ${param.name}: ${params}`;
+      return result;
     default:
-      return ''
+      return '';
   }
 }
 
